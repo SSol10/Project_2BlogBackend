418ca212b11f818029740e021dfd8ed5a80ebf67
SSol10 <130738808+SSol10@users.noreply.github.com>
2023-06-23 12:55:28 +0900

    Add files via upload

---
+++ schemas/comment.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  nickname: {
+    type: String,
+    ref: "User"
+  },
+  password: {
+    type: String,
+    ref: "User"
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: () => Date.now()
+  },
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post"
+  }
+});
+
+module.exports = mongoose.model("Comment", commentSchema);
